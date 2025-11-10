529d11f3914b5044f669ce3c2906907649862bbb
Javier Caride Ulloa <javier.caride@gmail.com>
2025-11-10 17:23:18 +0000

    Add populate_db.py file

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Deleting old data...')
+        Activity.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+
+        self.stdout.write('Creating teams...')
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        self.stdout.write('Creating users...')
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Captain America', email='cap@marvel.com', team=marvel),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+            User.objects.create(name='Superman', email='superman@dc.com', team=dc),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+        ]
+
+        self.stdout.write('Creating activities...')
+        Activity.objects.create(user=users[0], type='Running', duration=30, date=timezone.now().date())
+        Activity.objects.create(user=users[1], type='Cycling', duration=45, date=timezone.now().date())
+        Activity.objects.create(user=users[3], type='Swimming', duration=60, date=timezone.now().date())
+        Activity.objects.create(user=users[4], type='Yoga', duration=20, date=timezone.now().date())
+
+        self.stdout.write('Creating workouts...')
+        cardio = Workout.objects.create(name='Cardio Blast', description='High intensity cardio workout')
+        strength = Workout.objects.create(name='Strength Training', description='Build muscle and strength')
+        cardio.suggested_for.add(marvel, dc)
+        strength.suggested_for.add(dc)
+
+        self.stdout.write('Creating leaderboard...')
+        Leaderboard.objects.create(team=marvel, points=150)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        self.stdout.write(self.style.SUCCESS('Database populated with test data!'))
